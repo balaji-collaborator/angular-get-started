02ce29c5c1885b110541955b294e26fa4f0fbb6e
balaji <youcanemailbalaji@gmail.com>
2017-09-17 16:36:17 +0530

    Changed the text and the tests.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('angular-get-started App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to app works! Great start.');
   });
 });
